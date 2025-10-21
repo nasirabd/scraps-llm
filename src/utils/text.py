60fd579eebd261162d6ef82b37d21e5c21992ace
NasirAb <abdallah.41@osu>
2025-10-21 11:13:00 -0400

    feat: modified helper functions

--- src/utils/text.py
+++ src/utils/text.py
@@ -1,11 +1,17 @@
 import re
 #Look for one or more whitespace character (tabs, newline, spaces) in the string object 
 _whitespace = re.compile(r"\s+")
+_url = re.compile(r"https?://\S+")
+_bullets = re.compile(r"^[•\-\*]\s*", flags=re.MULTILINE)
 
 #Clean up whitespaces
 def clean(s:str) -> str:
     #remove leading and trailing whitespaces
     s = s.strip()
+    # remove URLs
+    s = _url.sub("", s)   
+     # drop leading bullets             
+    s = _bullets.sub("", s)            
     #replace the whitespace characters in s with a single space
     s = _whitespace.sub(" ", s) 
     return s
